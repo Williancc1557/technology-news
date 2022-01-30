42be7a13f9837e2ab3b9cab65fdd9256930ed3c1
Willian Cavalcanti Coelho <minegt1500@gmail.com>
2022-01-30 11:22:57 -0300

    one line updated

--- src/providers/IWebScrapingProvider.ts
+++ src/providers/IWebScrapingProvider.ts
@@ -2,7 +2,6 @@ export interface IWebScrapingProvider {
     getTechnologyNews: (requestNumber?: number) => Promise<object>;
 }
 
-
 export interface ITechnologyNewsResponse {
     title: string;
     paragraph: string;
